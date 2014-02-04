62211211ba9fff03a6864782d7da7d83461ba565
Ryan Poplin <mobileupstate@gmail.com>
2014-02-03 21:35:41 -0500

    added a lot...

--- app/scripts/greenvillejs.js
+++ app/scripts/greenvillejs.js
@@ -1,8 +1,41 @@
-(function() {
+/**
+*
+* Lesson Topics: 
+* JS Engine Scope Passes, Execution Context Objects, and Prototypes...
+*
+*/
 
-	var proto_type, Class_Hacker, class_hacker_one, class_hacker_two, test_object, js_hacker, http, i, j,node_hacker, urls;
+/**
+*
+* Shout Outs: The Iron Yard/ers, Mike Mikowski, Josh Powell, and MDN...
+* 
+*/
 
-	// a prototype...
+
+// 1st Pass: function args are the boss...
+// They can be optionally set a value before anything else in functional scope...
+(function(the_boss) {
+
+	// console.log(the_boss) === 'The Boss'...
+	// Not undefined...
+
+	// 1st Pass: local vars are all declared and initialized as 'undefined'...
+	var 
+		proto_type, 
+		test_object, 
+		Class_Hacker, 
+		class_hacker_one, 
+		class_hacker_two, 
+		js_hacker, 
+		http, 
+		i, 
+		j,
+		node_hacker, 
+		urls,
+		// this is redundant...
+		the_boss
+	;
+
 	proto_type = {
 		alias: 'Neck_Beard_001110',
 		get_alias: function() {
@@ -14,48 +47,37 @@
 		}
 	};
 
-	// a testing object...
 	test_object = {
 		alias: 'Test Object...'
 	};
 
-	// No more .__proto__ syntax...
 	Object.setPrototypeOf = Object.setPrototypeOf || function (obj, proto) {
   		obj.__proto__ = proto;
   		return obj; 
 	};
 
-	// 'test_object' prototype linkage...
 	Object.setPrototypeOf(test_object, proto_type);
 	/*console.log(test_object);
 	console.log(Object.getPrototypeOf(test_object));*/
 
-	// a class-like constructor function...
 	Class_Hacker = function(alias) {
 		this.alias = alias;
 	};
 
-	// Why .prototype for constructors?
-	// constructor prototype linkage...
 	Class_Hacker.prototype = proto_type;
 
-	// instantiate...
 	class_hacker_one = new Class_Hacker('Class_Based_Nihilus');
 
-	// logging...
 	/*console.log(class_hacker_one);
 	console.log(class_hacker_one.alias);
 	console.log(class_hacker_one.get_alias());*/
 
-	// instantiate...
 	class_hacker_two = new Class_Hacker();
 
-	// logging...
 	/*console.log(class_hacker_two);
 	console.log(Object.getPrototypeOf(class_hacker_two).alias);
 	console.log(Object.getPrototypeOf(class_hacker_two).get_alias());*/
 
-	// a prototype leading the way... 
 	js_hacker = Object.create(proto_type);
 	js_hacker.alias = 'No_Need_For_An_Alias';
 	js_hacker.abstraction = 'CoffeeScript';
@@ -64,8 +86,6 @@
 	console.log(js_hacker.alias);
 	console.log(js_hacker.abstraction);
 	console.log(js_hacker.get_alias());*/
-
-	// A little node app...
 
 	http = require('http');
 	urls = ['www.theironyard.com', 'www.naturalnews.com', 'www.youtube.com'];
@@ -77,10 +97,11 @@
 	node_hacker.wait_in_s = 'seconds utilizing Node...';
 	node_hacker.wait_in_s_con = function(ms) {
 		var seconds;
-		seconds = (ms / 1000) % 60;
+		seconds = ms / 1000;
 		return seconds;
 	};
 
+	// 1st Pass
 	function fetchPage(url) {
 		var start;
 		start = new Date();
@@ -93,9 +114,9 @@
 
 	for (i = 0; i < urls.length; i += 1) {
 		fetchPage(urls[i]);
-		for (j = 0; j < 4; j += 1) {
+		for (j = 0; j < 2; j += 1) {
 			fetchPage(urls[i]);
 		}
 	}
 
-})();
+})('The Boss...');
---
+++ app/scripts/parsebone.js
@@ -0,0 +1,122 @@
+/**
+* Full Stack User Interactions With Parse.com and Backbone.js...
+*/
+
+// Keys...
+Parse.initialize("tbIOLEXYyQMlsiZvdHpwpfxM1TfYf2AWuaY1ClAP", "PPL1IkNWuasv3kNjPuEA66dxQO1ZjR15kY2b7Tjj");
+
+(function($) {
+	
+	var app, user, router, core_router;
+
+	app = {
+		views: {},
+		router: null
+	};
+
+	app.views.index_view = Backbone.View.extend({
+		el: '#parsebone',
+		template: _.template($('#index-template').html()),
+		events: {
+			'click #log-in': 'log_in_func'
+		},
+		log_in_func: function() {
+			var username, password;
+			username = $('#log-in-username').val();
+			password = $('#log-in-password').val();
+			Parse.User.logIn(username, password, {
+				success: function(user) {
+					alert(user.get("email") + ' you have been logged in...');
+					core_router.navigate('profile', {trigger: true});
+				},
+				error: function(user, error) {
+					alert('An error has occured...');
+				}
+			});
+		},
+		initialize: function() {
+			console.log('index_view init...');
+		},
+		render: function() {
+			this.$el.html(this.template());
+			return this;
+		}
+	});
+
+	app.views.sign_up_view = Backbone.View.extend({
+		el: '#parsebone',
+		template: _.template($('#sign-up-template').html()),
+		events: {
+			'click #sign-up': 'sign_up_func'
+		},
+		sign_up_func: function() {
+			var username, email, password, user;
+			username = $('#sign-up-username').val();
+			email = $('#sign-up-email').val();
+			password = $('#sign-up-password').val();
+			user = new Parse.User();
+			user.set("username", username);
+			user.set("email", email);
+			user.set("password", password);
+			user.signUp(null, {
+				success: function(user) {
+					alert('Thank you for signing up!');
+					core_router.navigate('', {trigger: true});
+				},
+				error: function(user, error) {
+					alert("Error: " + error.code + " " + error.message);
+				}
+			});
+		},
+		initialize: function() {
+			console.log('sign_up_view init...');
+		},
+		render: function() {
+			this.$el.html(this.template());
+			return this;
+		}
+	});
+
+	app.views.profile_view = Backbone.View.extend({
+		el: '#parsebone',
+		template: _.template($('#profile-template').html()),
+		initialize: function() {
+			console.log('profile_view init...');
+		},
+		render: function() {
+			this.$el.html(this.template());
+			return this;
+		}
+	});
+
+	app.router = Backbone.Router.extend({
+		routes: {
+			'': 'index',
+			'sign-up': 'sign_up',
+			'profile': 'profile'
+		},
+		initialize: function() {
+			console.log('app.router init...');
+		},
+		index: function() {
+			var index_view;
+			index_view = new app.views.index_view();
+			index_view.render();
+		},
+		sign_up: function() {
+			var sign_up_view;
+			sign_up_view = new app.views.sign_up_view();
+			sign_up_view.render();
+		},
+		profile: function() {
+			var profile_view;
+			profile_view = new app.views.profile_view();
+			profile_view.render();
+		}
+	});
+
+	core_router = new app.router();
+
+	Backbone.history.start();
+
+})(jQuery);
